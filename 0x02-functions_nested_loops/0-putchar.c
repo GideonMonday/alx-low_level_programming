c81c6035b3083da7eb9d1b95ce3e16231c7b0ea4
gideonMonday <khamalkhalifa@gmail.com>
2023-08-15 05:41:28 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.c"
+/**
+* main - funtion that print putchar
+*
+* Return: return is 0
+*/
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
