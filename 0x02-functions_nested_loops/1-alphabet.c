2951e847dafe960e30dab800e34a02ab91401f49
gideonMonday <khamalkhalifa@gmail.com>
2023-08-15 07:07:50 -0700

    am done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - funtion to print alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; <= 'z'; i++)
+	{
+		_putchar (i);
+		_putchar ('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,8 @@
 #ifndef MAIN_C
 #define MAIN_C
+
 int _putchar(char c);
+
+void print_alphabet(void);
+
 #endif
