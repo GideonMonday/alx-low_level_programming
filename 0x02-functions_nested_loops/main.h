82b22c172e46c6192b3090450c3b3cbed5c5c0a6
gideonMonday <khamalkhalifa@gmail.com>
2023-08-16 07:19:53 -0700

    am done

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_to_98(int n);
+int print_last_digit(int);
 void print_alphabet(void);
 int _putchar(char c);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int)
 #endif
