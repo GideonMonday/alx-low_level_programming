a3fd9bcd11e01ed32173aa0ebcf40c52f20e87e4
gideonMonday <khamalkhalifa@gmail.com>
2023-08-22 12:10:01 -0700

    know ball

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,28 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * puts_half - Prints half of a string, followed by a new line
- * @str: The string to print
- */
+ * puts_half - prints half of a string
+ *
+ * @str: string parameter input
+ *
+ * Return: Nothing
+*/
+
 void puts_half(char *str)
 {
-	int length = str;
-	int start = (length + 1) / 2;
+	int l, n;
 
-	while (str[start] != '\0')
+	for (l = 0; str[l] != '\0'; ++l)
+		;
+
+	if (l % 2 == 0)
 	{
-		_putchar(str[start]);
-		start++;
+		for (n = l / 2; str[n] != '\0'; ++n)
+			_putchar(str[n]);
+	} else
+	{
+		for (n = ((l - 1) / 2) + 1; str[n] != '\0'; ++n)
+			_putchar(str[n]);
 	}
 	_putchar('\n');
 }
