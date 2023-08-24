1a36a2de3681a4c65250fe7f5388c0d5b602fcb4
gideonMonday <khamalkhalifa@gmail.com>
2023-08-24 12:18:50 -0700

    you see

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,52 +4,42 @@
 
 /**
  * cap_string - Capitalizes all words in a string.
- * is_separator: - Checks if a character is a separator.
- * @c: The character to check.
+ * @str: The input string to capitalize.
+ *
+ * Description:
+ *   This function capitalizes the first letter of each word in the given
+ *   string, considering certain separators. It also converts subsequent
+ *   letters of each word to lowercase.
+ *
+ * Prototype: char *cap_string(char *);
+ * Separators of words: space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }
+ *
  * Return: The capitalized string.
  */
-
-bool is_separator(char c)
-{
-	char separators[] = " \t\n,;.!?\"(){}";
-	int i;
-
-	for (i = 0; separators[i]; i++)
-	{
-		if (c == separators[i])
-		{
-			return (true);
-		}
-	}
-
-	return (false);
-}
-
 char *cap_string(char *str)
 {
 	bool new_word = true;
 	int i;
 
 	for (i = 0; str[i]; i++)
-
 	{
-		if (is_separator(str[i]))
-
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+		    str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			new_word = true;
 		}
-
 		else if (new_word)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
-
 			{
 				str[i] -= ('a' - 'A');
 			}
 			new_word = false;
 		}
 		else if (str[i] >= 'A' && str[i] <= 'Z')
-
 		{
 			str[i] += ('a' - 'A');
 		}
