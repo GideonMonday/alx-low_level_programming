d1dfc00923657d84af5209152cbccfa33c2e156c
gideonMonday <khamalkhalifa@gmail.com>
2023-08-29 15:07:17 -0700

    you commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
