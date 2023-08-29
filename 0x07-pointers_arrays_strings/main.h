7f7fb737556976a137f5e9bdb6e720212df6d20e
gideonMonday <khamalkhalifa@gmail.com>
2023-08-29 14:55:56 -0700

    you commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
