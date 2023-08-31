ce97bbd2b8d041f7c2a5679b07317814f962053c
gideonMonday <khamalkhalifa@gmail.com>
2023-08-31 15:14:16 -0700

    you commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints a string followed by a new line.
+ * @s: Pointer to the string to be printed.
+ *
+ * Return: None.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
