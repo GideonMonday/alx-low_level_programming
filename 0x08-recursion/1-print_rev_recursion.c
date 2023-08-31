66cfb3c997cf934ddaa8a495eb803d5131581898
gideonMonday <khamalkhalifa@gmail.com>
2023-08-31 15:20:07 -0700

    you commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: Pointer to the string to be printed in reverse.
+ *
+ * Return: None.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
