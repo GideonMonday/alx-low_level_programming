5075cf7d773ea301f1eb2661cb9bb7e8346d131d
gideonMonday <khamalkhalifa@gmail.com>
2023-08-31 15:34:02 -0700

    you commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 int factorial(int n);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
