3e08064b8956c23715fc410586e7d08ad048c0b5
gideonMonday <khamalkhalifa@gmail.com>
2023-09-04 08:39:33 -0700

    blessing

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments (including the program name)
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: (0) if successful,
+ * (1) if there are non-digit symbols or no arguments
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
